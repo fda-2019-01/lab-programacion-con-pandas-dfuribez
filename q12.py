b6f5b97832ac4abbba2c6e49a5f2e137c1edf0cc
Diego Uribe <dfuribez@unal.eu.co>
2019-08-27 10:22:08 -0500

    Solucionado

--- q12.py
+++ q12.py
@@ -4,4 +4,11 @@
 ## valor en tbl0._c1.
 ## 
 
+import pandas as pd 
 
+df0 = pd.read_csv("tbl0.tsv", sep="\t")
+df2 = pd.read_csv("tbl2.tsv", sep="\t")
+
+df = pd.merge(df0, df2, on="_c0")[["_c5a", "_c5b"]]
+
+print(df.groupby("_c5a").sum())
